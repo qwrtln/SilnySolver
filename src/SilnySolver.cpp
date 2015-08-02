bf40bee224c25395b2aab98f2060290b0de43a20
Andrzej Wiącek <wiacek.and@gmail.com>
2015-08-02 23:57:15 +0200

    First source file

---
+++ src/SilnySolver.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SilnySolver.cpp
+// Author      : PkozDelta
+// Version     : e-6
+// Copyright   : Solve anything you like
+// Description : Most probably Crazy 3x3x2
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "To jest Silny Solver." << endl; // prints To jest Silny Solver.
+	return 0;
+}
