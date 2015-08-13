7a52d0c772991189fcc8935d30358b68c0ac9372
Andrzej Wiącek <wiacek.and@gmail.com>
2015-08-13 17:53:32 +0200

    Created CrazyCube class and moved global code into

--- src/SilnySolver.cpp
+++ src/SilnySolver.cpp
@@ -6,6 +6,7 @@
 // Description : Most probably Crazy 3x3x2
 //============================================================================
 
+#include "CrazyCube.h"
 #include <iostream>
 #include <cstdio>
 using namespace std;
@@ -17,35 +18,30 @@ using namespace std;
 // to wartości w systemie binarnym, zapisywane na trzech bitach
 // Razem: 61 bitów
 //============================================================================
-const unsigned long long solvedCube = 0x102468ac13579bd;
 
-// Legalne ruchy
-enum rotation {
-	L,		// Left
-	F,		// Front
-	U,		// Up
-	Ui,		// Up inverted
-	U2, 	// U^2
-	Mv,		// Middle vertical
-	Mh, 	// Middle horizontal
-	MhRr,	// Middle horizontal Right rotation
-	MhLr,	// ...
-	MvFr,	// Middle vertical Front rotation
-	MvBr	// ...
-};
 
-bool checkIfSolved(unsigned long long cubeState)
-{
-	return (cubeState == solvedCube);
-}
+
 
 int main()
 {
+	CrazyCube kosteczka;
 	cout << "To jest Silny Solver." << endl;
 	cout << "A tak wyglada ulozona kostka:" << endl;
 	cout << "Hex: ";
-	printf("%llx\n",solvedCube);
+	printf("%llx\n",::solvedCube);
 	cout << "Dec: ";
-	printf("%llu\n",solvedCube);
+	printf("%llu\n",::solvedCube);
+
+	kosteczka.setCubeState(0x102468ac13579bdf);
+
+	//unsigned long long klockiNp[7] = {1,3,5,7,9,0xb,0xd};
+	//unsigned long long klockiP[7] = {0,2,4,6,8,0xa,0xc};
+	unsigned short klockiRandom[7] = {3,7,2,0xf,4,0xb,0xa};
+	cout << "Kosteczka: " << endl;
+	cout << "Hex: ";
+	printf("%llx\n",kosteczka.getCubeState());
+	cout << "Hex: ";
+	printf("%llx\n",::solvedCube);
+
 	return 0;
 }
