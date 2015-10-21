52288f0ead0c4a3fafb212824997783915f0edf1
Andrzej Wiącek <wiacek.and@gmail.com>
2015-10-21 21:06:45 +0200

    Moved all the 'production' code into new folder.

---
+++ Dev/inc/CrazyCube.h
@@ -0,0 +1,158 @@
+/*
+ * CrazyCube.h
+ *
+ *  Created on: Aug 13, 2015
+ *      Author: qwrtln
+ */
+  
+#ifndef CRAZYCUBE_H_
+#define CRAZYCUBE_H_
+#include "CrazyCubeAbstract.h"
+
+/**
+ *  3x3x2 CrazyCube representation.
+ */
+class CrazyCube: public CrazyCubeAbstract
+{
+private:
+	unsigned long long cubeState;
+	void(CrazyCube::*moveTab[NUM_OF_MOVES])();
+	void(CrazyCube::*undoMoveTab[NUM_OF_MOVES])();
+
+public:
+
+    /**
+     * Basic constructor.
+     */
+	CrazyCube();
+
+    /**
+     * Generic cube constructor
+     *
+     * @param genericCube. If != 0 genericCube is created. Otherwise normal constructor is called.
+     */
+	CrazyCube(int genericCube);
+
+    /**
+     * Virtual destructor.
+     */
+	virtual ~CrazyCube();
+
+    /**
+     * Checks if CrazyCube is solved.
+     *
+     * @return true if cube is solved. False if it is not.
+     */
+	bool checkIfSolved();
+
+    /**
+     * Checks if CrazyCube's inner circle is solved.
+     *
+     * @return true if inner circle is solved. False if it is not.
+     */
+	bool checkInnerCircle();
+
+    /**
+     * Checks if CrazyCube's edges are solved
+     *
+     * @return true if edges are solved. False if it is not.
+     */
+	bool checkEdges();
+
+    /**
+     * Checks if CrazyCube's corners are solved
+     *
+     * @return true if corners are solved. False if it is not.
+     */
+	bool checkCorners();
+
+    /**
+     * Checks if CrazyCube is solved with respect to provided mask
+     *
+     * @param solvedMask parts of CrazyCube expected to be solved
+     * @return true if indeed our CrazyCube is solved. False if it is unfortunately not.
+     */
+	bool checkIfSolved(unsigned long long int solvedMask);
+
+    /**
+     * Sets CrazyCube state
+     *
+     * @param cubeState state to which our cube is about to be set
+     */
+	void setCubeState(unsigned long long cubeState);
+
+    /**
+     * Gets CrazyCube state
+     *
+     * @return CrazyCube state as an unsigned long long int value
+     */
+	unsigned long long getCubeState();
+
+    /**
+     * Sets CrazyCube centre
+     *
+     * @param centres state
+     */
+	void setCentre(bool);
+
+    /**
+     * Sets CrazyCube edges as an array of edgeNames
+     *
+     * @param edges array of edgeNames
+     */
+	void setEdges(edgeNames edges[NUM_OF_EDGES]);
+
+    /**
+     * Sets CrazyCube corners as an array of cornerNames
+     *
+     * @param corners array of cornerNames
+     */
+	void setCorners(cornerNames corners[NUM_OF_CORNERS]);
+
+    /**
+     * Sets the whole cube to state provided in separate arrays
+     *
+     * @param edges see above
+     * @param corners see above
+     * @param centre see above
+     */
+	void setWholeCube(bool, edgeNames edges[NUM_OF_EDGES], cornerNames corners[NUM_OF_CORNERS]);
+
+    /**
+     * CrazyCube moves implementation
+     */
+	void L();
+	void F();
+	void U();
+	void Ui();
+	void U2();
+	void Mv();
+	void Mh();
+	void MhRr();
+	void MhLr();
+	void MvFr();
+	void MvBr();
+
+    /**
+     * CrazyCube move implementation using move index
+     *
+     * @param m index of move to be done
+     */
+	void move(unsigned short int m);
+		/**
+     * Inversion of a provided move
+     *
+     * @param m index of move to be undone
+     */
+	void undoMove(unsigned short int m);
+		/**
+     * Toggles the cube centre orientation
+     */
+	void toggleCentre();
+		/**
+     * Cube is solved again with no effort :)
+     */
+	void resetCube();
+};
+
+#endif /* CRAZYCUBE_H_ */
---
+++ Dev/inc/CrazyCubeAbstract.h
@@ -0,0 +1,84 @@
+/*
+ * CrazyCubeAbstract.h
+ *
+ *  Created on: Aug 13, 2015
+ *      Author: qwrtln
+ */
+
+#ifndef CONSTDEFS_H_
+#define CONSTDEFS_H_
+#define NUM_OF_EDGES 8
+#define NUM_OF_CORNERS 7
+#define NUM_OF_MOVES 11
+
+class CrazyCubeAbstract
+{
+public:
+	const static unsigned long long solvedCube;
+	const static unsigned long long solvedInnerCircle;
+	const static unsigned long long solvedCentre;
+	const static unsigned long long solvedEdges;
+	const static unsigned long long solvedInnerEdges;
+	const static unsigned long long solvedCorners;
+	const static unsigned long long solvedInnerCorners;
+
+	const static unsigned short int CornerPieces[NUM_OF_CORNERS];
+	const static unsigned short int EdgePieces[NUM_OF_EDGES];
+	const static unsigned short int centrePosition;
+
+	// Legalne ruchy < 11
+    enum rotation {
+		L,		// Left 0
+		F,		// Front
+		U,		// Up
+		Ui,		// Up inverted
+		U2, 	// U^2
+		Mv,		// Middle vertical
+		Mh, 	// Middle horizontal
+		MhRr,	// Middle horizontal Right rotation
+		MhLr,	// ...
+		MvFr,	// Middle vertical Front rotation
+		MvBr	// ... 10
+	};
+
+	// Nazwy rogow
+	enum cornerNames {
+		ygry = 0x0,	// yellow green red (inner yellow)
+		ygrw = 0x1,	// yellow green red (inner white)
+		yrby = 0x2,	// etc
+		yrbw = 0x3,	//...
+		yboy = 0x4,
+		ybow = 0x5,
+		yogy = 0x6,
+		yogw = 0x7,
+		wrgy = 0x8,	// white red green (inner yellow)
+		wrgw = 0x9,	// white red green (inner white)
+		wbry = 0xA,	// etc
+		wbrw = 0xB,	// ...
+		woby = 0xC,
+		wobw = 0xD
+	};
+
+	// Nazwy krawedzi
+	enum edgeNames {
+		yry = 0x0,	// yellow red (inner yellow)
+		yrw = 0x1,	// yellow red (inner white)
+		yby = 0x2,	// etc
+		ybw = 0x3,
+		yoy = 0x4,
+		yow = 0x5,
+		ygy = 0x6,
+		ygw = 0x7,
+		wry = 0x8,	// white red (inner yellow)
+		wrw = 0x9,	// white red (inner white)
+		wby = 0xA,	// etc
+		wbw = 0xB,
+		woy = 0xC,
+		wow = 0xD,
+		wgy = 0xE,
+		wgw = 0xF
+	};
+};
+
+
+#endif /* CONSTDEFS_H_ */
