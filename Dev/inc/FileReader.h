b3037c657dc30baac50903ff0138af71f4264a86
SecT <grzegorzstarowicz@gmail.com>
2015-12-02 21:19:28 +0100

    Feature fileReader
    Added declarations for readArray, getArray.
    Added mock definitions for readArray, getArray.
    
    Added fields: array, arraySize
    
    Algorithm for reading numbers from file - in progress

---
+++ Dev/inc/FileReader.h
@@ -0,0 +1,33 @@
+#include <fstream>
+//#include <string>
+
+
+//using std::string;
+using std::ifstream;
+
+class FileReader{
+
+
+ifstream file;
+
+int* array;
+
+int arraySize;
+
+
+public:
+
+FileReader(char* fileName);
+
+
+void readArray(char delimiter = ' ');
+
+int* getArray();
+
+int getArraySize();
+
+
+~FileReader();
+
+
+};
---
+++ Dev/src/FileReader.cpp
@@ -0,0 +1,124 @@
+#include "FileReader.h"
+
+#include <string>
+
+#include <cstring>
+#include <cstdlib>
+
+//#include <fstream>
+
+//using std::ifstream;
+
+
+FileReader::FileReader(char* fileName)
+{
+
+file.open(fileName, std::ifstream::in);
+
+array = NULL;
+
+}
+
+void FileReader::readArray(char delimiter) //delimiter = ' '
+{
+
+//tmp
+arraySize = 1;
+//tmp
+
+//TODO   dynamic buffer size
+#define TOKEN_BUFFER_SIZE 3
+char token[TOKEN_BUFFER_SIZE];    
+//
+
+//short tokenLength = 0;
+
+int currentArraySize = 0;
+
+//first parse the file to get the required array size
+
+while(!file.eof() )
+{
+	char currentChar;
+
+	file.get(currentChar);
+
+
+	if(currentChar == delimiter)
+	{
+	currentArraySize++;			
+	}
+}
+
+
+//allocate the array
+array = new int[arraySize];
+
+
+//parse the file again, this time writing the numbers to the array
+
+int currentTokenId = 0;
+
+int currentTokenLength = 0;
+
+while(!file.eof())
+{
+	char currentChar;
+
+	file.get(currentChar);
+
+	if(currentChar == delimiter)
+	{
+	
+	//convert the token to an integer and write it to the array
+
+
+	
+	//mock
+
+	strcpy(token, "27");
+	//	
+
+	array[currentTokenId] = atoi(token);
+
+
+	//reset token
+	for(int i = 0; i<TOKEN_BUFFER_SIZE; i++)
+	{
+	token[i] = '\0';
+	}	
+
+
+	currentTokenId++;
+
+	}
+	else
+	{
+
+	token[currentTokenLength] = currentChar;
+
+	currentTokenLength++;
+
+	}
+
+}
+
+}
+
+int* FileReader::getArray()
+{
+return array; 
+}
+
+int FileReader::getArraySize()
+{
+return arraySize;
+}
+
+
+FileReader::~FileReader()
+{
+
+file.close();
+
+
