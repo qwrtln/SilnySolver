136ec65006501facff9321c3767eb751d49a5f57
SecT <grzegorzstarowicz@gmail.com>
2016-02-10 19:15:22 +0100

    IN PROGRESS. Writes array to file. Linker error: relocation truncated to fit

--- Dev/src/FileWriter.cpp
+++ Dev/src/FileWriter.cpp
@@ -1,6 +1,54 @@
-#include "FileWriter.h"
+#include <FileWriter.h>
+
+#include <fstream>
+
+using std::ofstream;
 
 
+FileWriter::FileWriter()
+{
 
+file.open("data.txt", std::fstream::in);
+
+}
+
+FileWriter::FileWriter(char const* fileName)
+{
+
+}
+
+
+void FileWriter::writeArray(char delimiter)
+{
+
+if (isOpen())
+{
+
+for(int i=0; i<arraySize; i++)
+{
+
+
+file<<array[i]<<delimiter;
+
+}
+
+}
+
+}
+
+
+bool FileWriter::isOpen()
+{
+
+if(!file) return false;
+
+return true;
+
+}
+
+FileWriter::~FileWriter()
+{
+
+}
 
 
