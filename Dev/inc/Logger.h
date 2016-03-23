b65b3166d5bfec7639dd7f9a41d49de36c47a390
SecT <grzegorzstarowicz@gmail.com>
2016-03-23 07:48:36 +0100

    Added Logger

---
+++ Dev/inc/Logger.h
@@ -0,0 +1,13 @@
+class Logger{
+
+	private:
+	
+	static void writeToFile(char* fileName);
+	static void writeToConsole();
+
+
+	public:
+
+	static void log(char* text);
+
+};
---
+++ Dev/src/Logger.cpp
@@ -0,0 +1,16 @@
+#include "Logger.h"
+
+
+void Logger::writeToFile(char* fileName)
+{	
+	}
+
+void Logger::writeToConsole()
+{
+
+}
+
+void Logger::log(char* text)
+{
+
+}
