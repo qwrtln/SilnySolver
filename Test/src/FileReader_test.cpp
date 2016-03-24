bde5b17e66bc15e09b48f661250f829c7a40ff74
Grzegorz Starowicz <grzegorz.starowicz@gmail.com>
2016-03-24 11:33:12 +0100

    Added test cases

--- Test/src/FileReader_test.cpp
+++ Test/src/FileReader_test.cpp
@@ -6,7 +6,6 @@
  */
 #include "FileReader.h"
 #include "TestExecuter.h"
-//#include "CrazyCubeAbstract.h"
 #include <cmath>
 
 #include <fstream>
@@ -30,28 +29,12 @@ protected:
 	void SetUp()
 	{
 
-	ofstream testFile;
-
-	testFile.open("./testFileReader.txt");		
-
-	testFile<<"10 20";
-
-	testFile.close();
-
-
-	//fileReader = new FileReader("./testFileReader.txt");
-
-
-	fileReader.open("./testFileReader.txt");
 
 	}
 
 	void TearDown()
-	{
+	{	
 
-	
-	remove("./testFileReader.txt");
-	//delete	fileReader;
 
 	}
 
@@ -63,12 +46,184 @@ protected:
 
 FileReader FileReaderTest::fileReader;
 
-TEST_F(FileReaderTest, readNumbersFromFile)
+TEST_F(FileReaderTest, basicRead)
 {
+
+	ofstream testFile;
+
+	testFile.open("./testFileReader.txt");		
+
+	testFile<<"1 2 6";
+
+	testFile.close();
+
+
+
+
+	fileReader.open("./testFileReader.txt");
+
+
+
 	FileReader fileReader("./testFileReader.txt");		
 
 
+	fileReader.readArrayFromFile();
+	
+	int* myArray = fileReader.getArray();
+	int myArraySize = fileReader.getArraySize();
 
+	ASSERT_EQ(myArraySize, 3);
+
+	ASSERT_EQ(myArray[0], 1);
+	
+	ASSERT_EQ(myArray[1], 2);
+
+	ASSERT_EQ(myArray[2], 6);
+
+
+	remove("./testFileReader.txt");
+
+}
+
+
+
+TEST_F(FileReaderTest, doubleDigits)
+{
+
+	ofstream testFile;
+
+	testFile.open("./testFileReader.txt");		
+
+	testFile<<"50 20 40";
+
+	testFile.close();
+
+
+
+
+	fileReader.open("./testFileReader.txt");
+
+
+
+	FileReader fileReader("./testFileReader.txt");		
+
+
+	fileReader.readArrayFromFile();
+	
+	int* myArray = fileReader.getArray();
+	int myArraySize = fileReader.getArraySize();
+
+	ASSERT_EQ(myArraySize, 3);
+
+	ASSERT_EQ(myArray[0], 50);
+	
+	ASSERT_EQ(myArray[1], 20);
+
+	ASSERT_EQ(myArray[2], 40);
+
+
+	remove("./testFileReader.txt");
+
+}
+
+
+TEST_F(FileReaderTest, tripleDigits)
+{
+
+	ofstream testFile;
+
+	testFile.open("./testFileReader.txt");		
+
+	testFile<<"500 245 471";
+
+	testFile.close();
+
+
+
+
+	fileReader.open("./testFileReader.txt");
+
+
+
+	FileReader fileReader("./testFileReader.txt");		
+
+
+	fileReader.readArrayFromFile();
+	
+	int* myArray = fileReader.getArray();
+	int myArraySize = fileReader.getArraySize();
+
+	ASSERT_EQ(myArraySize, 3);
+
+	ASSERT_EQ(myArray[0], 500);
+	
+	ASSERT_EQ(myArray[1], 245);
+
+	ASSERT_EQ(myArray[2], 471);
+
+
+	remove("./testFileReader.txt");
+
+}
+
+TEST_F(FileReaderTest, variableDigits)
+{
+
+	ofstream testFile;
+
+	testFile.open("./testFileReader.txt");		
+
+	testFile<<"10 2 345";
+
+	testFile.close();
+
+
+
+
+	fileReader.open("./testFileReader.txt");
+
+
+
+	FileReader fileReader("./testFileReader.txt");		
+
+
+	fileReader.readArrayFromFile();
+	
+	int* myArray = fileReader.getArray();
+	int myArraySize = fileReader.getArraySize();
+
+	ASSERT_EQ(myArraySize, 3);
+
+	ASSERT_EQ(myArray[0], 10);
+	
+	ASSERT_EQ(myArray[1], 2);
+
+	ASSERT_EQ(myArray[2], 345);
+
+
+	remove("./testFileReader.txt");
+
+}
+
+
+TEST_F(FileReaderTest, emptyFile)
+{
+
+	ofstream testFile;
+
+	testFile.open("./testFileReader.txt");		
+
+
+	testFile.close();
+
+
+
+
+	fileReader.open("./testFileReader.txt");
+
+
+
+	FileReader fileReader("./testFileReader.txt");		
 
 
 	fileReader.readArrayFromFile();
@@ -76,8 +231,180 @@ TEST_F(FileReaderTest, readNumbersFromFile)
 	fileReader.getArray();
 	int myArraySize = fileReader.getArraySize();
 
-	ASSERT_EQ(myArraySize, 2);
+	ASSERT_EQ(myArraySize, 0);
+
+
+	remove("./testFileReader.txt");
+
 }
 
 
+
+TEST_F(FileReaderTest, spaceAtEndOfFile)
+{
+
+	ofstream testFile;
+
+	testFile.open("./testFileReader.txt");		
+
+	testFile<<"10 20 1 ";
+
+	testFile.close();
+
+
+
+
+	fileReader.open("./testFileReader.txt");
+
+
+
+	FileReader fileReader("./testFileReader.txt");		
+
+
+	fileReader.readArrayFromFile();
+	
+	int* myArray = fileReader.getArray();
+	int myArraySize = fileReader.getArraySize();
+
+	ASSERT_EQ(myArraySize, 3);
+
+	ASSERT_EQ(myArray[0], 10);
+	
+	ASSERT_EQ(myArray[1], 20);
+
+	ASSERT_EQ(myArray[2], 1);
+
+
+	remove("./testFileReader.txt");
+
+}
+
+
+
+TEST_F(FileReaderTest, spaceAtStartOfFile)
+{
+
+	ofstream testFile;
+
+	testFile.open("./testFileReader.txt");		
+
+	testFile<<" 10 20 1";
+
+	testFile.close();
+
+
+
+
+	fileReader.open("./testFileReader.txt");
+
+
+
+	FileReader fileReader("./testFileReader.txt");		
+
+
+	fileReader.readArrayFromFile();
+	
+	int* myArray = fileReader.getArray();
+	int myArraySize = fileReader.getArraySize();
+
+	ASSERT_EQ(myArraySize, 3);
+
+	ASSERT_EQ(myArray[0], 10);
+	
+	ASSERT_EQ(myArray[1], 20);
+
+	ASSERT_EQ(myArray[2], 1);
+
+
+	remove("./testFileReader.txt");
+
+}
+
+
+TEST_F(FileReaderTest, readArrayAfterRemovingFile)
+{
+
+	ofstream testFile;
+
+	testFile.open("./testFileReader.txt");		
+
+	testFile<<"50 20 40";
+
+	testFile.close();
+
+
+
+
+	fileReader.open("./testFileReader.txt");
+
+
+
+	FileReader fileReader("./testFileReader.txt");		
+
+
+	fileReader.readArrayFromFile();
+	
+
+	remove("./testFileReader.txt");
+
+	int* myArray = fileReader.getArray();
+	int myArraySize = fileReader.getArraySize();
+
+	ASSERT_EQ(myArraySize, 3);
+
+	ASSERT_EQ(myArray[0], 50);
+	
+	ASSERT_EQ(myArray[1], 20);
+
+	ASSERT_EQ(myArray[2], 40);
+
+
+
+
+}
+
+
+
+TEST_F(FileReaderTest, readArrayAfterDeletingReader)
+{
+
+	ofstream testFile;
+
+	testFile.open("./testFileReader.txt");		
+
+	testFile<<"50 20 40";
+
+	testFile.close();
+
+
+
+
+	fileReader.open("./testFileReader.txt");
+
+
+
+	FileReader* fileReader = new FileReader("./testFileReader.txt");		
+
+
+	fileReader->readArrayFromFile();
+	
+	delete fileReader;
+
+
+	int* myArray = fileReader->getArray();
+	int myArraySize = fileReader->getArraySize();
+
+	ASSERT_EQ(myArraySize, 3);
+
+	ASSERT_EQ(myArray[0], 50);
+	
+	ASSERT_EQ(myArray[1], 20);
+
+	ASSERT_EQ(myArray[2], 40);
+
+
+
+	remove("./testFileReader.txt");
+
+}
 
