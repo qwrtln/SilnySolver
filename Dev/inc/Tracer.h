700814284918ff11a75b3abdd7ba4afe1dc5cda0
SecT <grzegorzstarowicz@gmail.com>
2016-04-05 19:55:29 +0200

    Trace message is now formatted

--- Dev/inc/Tracer.h
+++ Dev/inc/Tracer.h
@@ -1,2 +1,9 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
 #define TRACE(OBJECT, LEVEL, TEXT, ...) \
-	std::cout<<TEXT<<endl;
+	cout<<OBJECT<<LEVEL<<":"<<TEXT<<endl;
+
+#define TRACE_ALL "TRACE_ALL"
