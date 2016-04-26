e5bf1508813e0a5c7ac92fe8a3b64dcb51b88838
SecT <tajnyt@gmail.com>
2016-04-26 18:22:13 +0200

    [Tracing] IN PROGRESS: tracing levels
    Change-Id: Ia1e1afc34411934e6fcb218f938782a39414e5d3

--- Tracing/inc/Tracer.h
+++ Tracing/inc/Tracer.h
@@ -3,8 +3,16 @@
 #include <stdio.h>
 
 #ifdef ACT_TRACE_ALL
+
+#ifdef ACT_TRACELEVEL
+
+const int activatedTraceLevel = #ACT_TRACELEVEL;
+#endif
+
 #define TRACE(OBJECT, LEVEL, TEXT, ...) \
-	printf(#OBJECT " " #LEVEL ":" TEXT "\n" , ##  __VA_ARGS__)
+	if(activatedTraceLevel == #LEVEL){\
+	printf(#OBJECT " " #LEVEL ":" TEXT "\n" , ##  __VA_ARGS__)\
+	}
 
 #else	
 #define TRACE(OBJECT, LEVEL, TEXT, ...) 
