444bf61e6346d800c6cd3a50a2a88605716eba44
Andrzej Wiacek <Andrzej.Wiacek@ericsson.com>
2016-04-29 13:52:05 +0200

    [DevTools] Removed sample hooks from dev tools.
    Change-Id: I28e17875408cba846f2c72ff08989de7bf47f856

--- .devTools/set.sh
+++ .devTools/set.sh
@@ -3,5 +3,4 @@
 # the development process.
 
 git config --global --unset alias.deliver # Legacy alias - deprecated
-mv .git/hooks/*\.sample .git/hooks/defaults/ -f # Backup in case default hooks are needed
 cp .devTools/pre-push .git/hooks/ # Set delivery check script
