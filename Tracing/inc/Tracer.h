c24f48d969d3bae044246ca15836b504ab9b7b82
SecT <tajnyt@gmail.com>
2016-05-04 20:42:19 +0200

    [Tracing] Added trace levels
    Change-Id: Iadfbeaf83f70b4ffd0e3e026b9fb52813567bfc6

--- Tracing/inc/Tracer.h
+++ Tracing/inc/Tracer.h
@@ -4,14 +4,20 @@
 
 #ifdef ACT_TRACE_ALL
 
+
 #ifdef ACT_TRACELEVEL
 
-const int activatedTraceLevel = #ACT_TRACELEVEL;
+const int activatedTraceLevel =  ACT_TRACELEVEL;
+#else
+
+const int activatedTraceLevel =  -1;
+
 #endif
 
 #define TRACE(OBJECT, LEVEL, TEXT, ...) \
-	if(activatedTraceLevel == #LEVEL){\
-	printf(#OBJECT " " #LEVEL ":" TEXT "\n" , ##  __VA_ARGS__)\
+	if(activatedTraceLevel == LEVEL \
+			|| activatedTraceLevel == -1){\
+	printf(#OBJECT " " #LEVEL ":" TEXT "\n" , ##  __VA_ARGS__);\
 	}
 
 #else	
