68a56807ba8a3d56851c3f65dad6552816f18492
Andrzej Wiącek <wiacek.and@gmail.com>
2016-05-15 11:50:57 +0200

    [DevTools] Added script to append Change-Id to merging commits.
    Change-Id: Icbd98217ba12a8d1204b44eb69ad61b595018eae

--- .devTools/set.sh
+++ .devTools/set.sh
@@ -2,7 +2,28 @@
 # The purpose of this script is to set up Git hooks, which facilitate
 # the development process.
 
-git config --global --unset alias.deliver # Legacy alias - deprecated
-cp .devTools/pre-push .git/hooks/ # Set delivery check script
+hooks=(commit-msg  post-checkout  prepare-commit-msg  pre-push)
 
-git config alias.set '!bash .devTools/set.sh'
+UPDATED=true
+
+for i in "${hooks[@]}"  # Iterate over hooks to see if they are up-to-date
+do
+    if ! diff ".devTools/hooks/$i" ".git/hooks/$i" >/dev/null
+    then
+        UPDATED=false
+    fi
+done
+
+if [ "$UPDATED" = true ]
+then
+    echo -e "Hooks are already up to date."
+else
+    cp -r .devTools/hooks .git/                  # Copy all the scripts
+    echo -e "Successfully copied the hooks."
+fi
+
+git config alias.set '!bash .devTools/set.sh'    # Set some aliases
+git config alias.last 'log -1 HEAD'
+git config alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config alias.lgd "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
+git config alias.aliases "!git config --get-regexp 'alias.*' | colrm 1 6 | sed 's/[ ]/ = /' "
