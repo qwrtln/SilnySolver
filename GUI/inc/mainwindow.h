4c268568de4ea0374f3b42f8cbe49dbaa66dc08a
SecT <tajnyt@gmail.com>
2016-06-02 22:03:07 +0200

    [GUI] Added events for gui elements: -move buttons -min/max depth setting -number of solutions setting
    Change-Id: I3c86b0c71d0e4a5897e40830b74ac1fd2b2de7c3

--- GUI/inc/mainwindow.h
+++ GUI/inc/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "../Dev/inc/CrazyCubeSolver.h"
 
 namespace Ui {
 class MainWindow;
@@ -20,8 +21,43 @@ private slots:
 
     void on_solveButton_clicked();
 
+    void on_depthInputBox_valueChanged(int arg1);
+
+    void on_depthMinInputBox_valueChanged(int val);
+
+    void on_depthMaxInputBox_valueChanged(int val);
+
+    void on_numberOfSolutionsSpinBox_valueChanged(int val);
+
+    void on_LMoveButton_clicked();
+
+    void on_FMoveButton_clicked();
+
+    void on_UMoveButton_clicked();
+
+    void on_UReverseMoveButton_clicked();
+
+    void on_U2MoveButton_clicked();
+
+    void on_MVMoveButton_clicked();
+
+    void on_MHRMoveButton_clicked();
+
+    void on_MHMoveButton_clicked();
+
+    void on_MHLMoveButton_clicked();
+
+    void on_MVFMoveButton_clicked();
+
+    void on_MVBMoveButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+//SilnySolver
+    CrazyCubeSolver mySolver;
+    CrazyCube myCube;
+
 };
 
 #endif // MAINWINDOW_H
--- GUI/src/mainwindow.cpp
+++ GUI/src/mainwindow.cpp
@@ -1,7 +1,7 @@
 #include "inc/mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include "../Dev/inc/CrazyCube.h"
+
 #include "../Dev/inc/CrazyCubeImproved.h"
 #include "../Dev/inc/CrazyCubeSolver.h"
 
@@ -10,6 +10,14 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //prepare solver
+    mySolver.setCrazyCube(&myCube);
+
+    mySolver.setMinDepth(0);
+    mySolver.setMaxDepth(0);
+
+    mySolver.setNumberOfSolutions(0);
 }
 
 MainWindow::~MainWindow()
@@ -20,47 +28,38 @@ MainWindow::~MainWindow()
 
 QString translateMove(unsigned short move)
 {
-      switch(move)
-      {
-          case 0: return "LEFT ";
-      case 1: return "FRONT ";
-         case 2: return "UP ";
-         case 3: return "UP_INVERTED ";
-      case 4: return "UP_2 ";
-         case 5: return "MIDDLE_VERTICAL ";
-         case 6: return "MIDDLE_HORIZONTAL ";
-
-       case 7: return "MIDDLE_HORIZONTAL_RIGHT_ROTATION ";
-      case 8: return "MIDDLE_HORIZONTAL_LEFT_ROTATION ";
-         case 9: return "MIDDLE_VERTICAL_FRONT_ROTATION ";
-
-
-          case 10: return "MIDDLE_VERTICAL_BACK_ROTATION ";
-      }
-        return "";
- }
+    switch(move)
+    {
+        case 0: return "LEFT ";
+        case 1: return "FRONT ";
+        case 2: return "UP ";
+        case 3: return "UP_INVERTED ";
+        case 4: return "UP_2 ";
+        case 5: return "MIDDLE_VERTICAL ";
+        case 6: return "MIDDLE_HORIZONTAL ";
+        case 7: return "MIDDLE_HORIZONTAL_RIGHT_ROTATION ";
+        case 8: return "MIDDLE_HORIZONTAL_LEFT_ROTATION ";
+        case 9: return "MIDDLE_VERTICAL_FRONT_ROTATION ";
+        case 10: return "MIDDLE_VERTICAL_BACK_ROTATION ";
+        default: return "";
+    }
+}
 
 
 
 void MainWindow::on_solveButton_clicked()
 {
 
-    CrazyCubeSolver solver;
-    CrazyCube myCube;
 
-    myCube.move(CrazyCube::rotation::MIDDLE_VERTICAL_BACK_ROTATION);
-    myCube.move(CrazyCube::rotation::LEFT);
 
-    solver.setCrazyCube(&myCube);
+    //myCube.move(CrazyCube::rotation::MIDDLE_VERTICAL_BACK_ROTATION);
+    //myCube.move(CrazyCube::rotation::LEFT);
 
-    solver.setMinDepth(1);
-    solver.setMaxDepth(3);
-    solver.setNumberOfSolutions(1);
 
-    solver.solve();
+    mySolver.solve();
 
     vector<vector<unsigned short int> > rozwiazania;
-    rozwiazania = solver.getSolutions();
+    rozwiazania = mySolver.getSolutions();
 
     QString solutionText;
 
@@ -72,7 +71,7 @@ void MainWindow::on_solveButton_clicked()
              {
                  solutionText = solutionText + "\n" + translateMove(rozwiazania[i][j]);
              }
-             //cout << endl;
+
          }
 
 
@@ -82,4 +81,81 @@ void MainWindow::on_solveButton_clicked()
 void MainWindow::on_playButton_clicked()
 {
 
+}
+
+
+//TODO: remove
+void MainWindow::on_depthInputBox_valueChanged(int arg1)
+{
+    mySolver.setMinDepth(1);
+}
+
+void MainWindow::on_depthMinInputBox_valueChanged(int val)
+{
+    mySolver.setMinDepth(val);
+}
+
+void MainWindow::on_depthMaxInputBox_valueChanged(int val)
+{
+    mySolver.setMaxDepth(val);
+}
+
+void MainWindow::on_numberOfSolutionsSpinBox_valueChanged(int val)
+{
+    mySolver.setNumberOfSolutions(val);
+}
+
+void MainWindow::on_LMoveButton_clicked()
+{
+    myCube.move(CrazyCube::rotation::LEFT);
+}
+
+void MainWindow::on_FMoveButton_clicked()
+{
+    myCube.move(CrazyCube::rotation::FRONT);
+}
+
+void MainWindow::on_UMoveButton_clicked()
+{
+    myCube.move(CrazyCube::rotation::UP);
+}
+
+void MainWindow::on_UReverseMoveButton_clicked()
+{
+    myCube.move(CrazyCube::rotation::UP_INVERTED);
+}
+
+void MainWindow::on_U2MoveButton_clicked()
+{
+    myCube.move(CrazyCube::rotation::UP_2);
+}
+
+void MainWindow::on_MVMoveButton_clicked()
+{
+    myCube.move(CrazyCube::rotation::MIDDLE_VERTICAL);
+}
+
+void MainWindow::on_MHRMoveButton_clicked()
+{
+    myCube.move(CrazyCube::rotation::MIDDLE_HORIZONTAL_RIGHT_ROTATION);
+}
+
+void MainWindow::on_MHMoveButton_clicked()
+{
+    myCube.move(CrazyCube::rotation::MIDDLE_HORIZONTAL);
+}
+
+void MainWindow::on_MHLMoveButton_clicked()
+{
+    myCube.move(CrazyCube::rotation::MIDDLE_HORIZONTAL_LEFT_ROTATION);
+}
+
+void MainWindow::on_MVFMoveButton_clicked()
+{
+    myCube.move(CrazyCube::rotation::MIDDLE_VERTICAL_FRONT_ROTATION);
+}
+
+void MainWindow::on_MVBMoveButton_clicked()
+{
+    myCube.move(CrazyCube::rotation::MIDDLE_VERTICAL_BACK_ROTATION);
 }
